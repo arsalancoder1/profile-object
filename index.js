ad66d3457bad4b6928d1e30ef72550c1e90ac09a
Arsalan Zubairi <syco5839@gmail.com>
2026-07-15 16:50:06 +0500

    Update project files

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-
 const product = {
-
-    title : "Parker Jotter Standard Ball pen",
-    colour : "Black",
-    rating : "7 star",
-    price : 270
-
+  title: "Parker Jotter Standard Ball pen",
+  colour: "Black",
+  rating: "7 star",
+  price: 270,
 };
 
-console.log(product)
+console.log(product);
+
+let user = Number(prompt("Enter a Number"));
+
+if (user % 5) {
+  console.log("Multiple of 5");
+} else {
+  console.log("Not a Multiple of 5");
+}
