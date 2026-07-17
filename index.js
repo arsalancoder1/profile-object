5bc1cc081051ee575c074c5948792e5c61892c5a
Arsalan Zubairi <syco5839@gmail.com>
2026-07-17 21:06:07 +0500

    Update grading score ranges

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ if (user % 5) {
 } else {
   console.log("Not a Multiple of 5");
 }
+
+let score = 45 ;
+if (score >= 95) {
+  console.log("Grade A");
+} else if (score >= 75) {
+  console.log("Grade B");
+} else if (score >= 65) {
+  console.log("Grade C");
+} else if (score >= 50) {
+  console.log("Grade D");
+} else {
+  console.log("Grade F");
+}
+
+
